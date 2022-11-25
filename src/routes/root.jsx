81d77e6885b354556da206c42970f0f6ba5b1d0e
storh1118 <storh1118@outlook.com>
2022-11-25 21:25:53 +0800

    捕获路由错误并显示报错页面

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,20 @@
 export default function Root() {
+
+    // 注意，errorElement 只能捕获一部分错误，并不是全部的错误
+    // 主要处理这几种情况：
+    // 1. React Router 所提供的功能中抛出的错误，代表就是 loaders 和 actions
+    // 2. 路由不匹配的情况，如这个页面中 href 跳转的两个不存在的路由
+    // 3. 渲染过程中出现的错误，如下面的代码。通过点击抛出的错误并不能被捕获，
+    //    但是直接在 render 内容中抛出错误的话会被捕捉
+
+    // 不会触发 errorElement
+    const functionError = () => {
+        throw new Error('自定义错误')
+    }
+
+    // 会触发 errorElement
+    // throw new Error('自定义错误')
+
     return (
         <>
             <div id="sidebar">
@@ -33,6 +49,9 @@ export default function Root() {
                         </li>
                         <li>
                             <a href={`contacts/2`}>Your Friend</a>
+                        </li>
+                        <li>
+                            <div style={{ cursor: "pointer" }} onClick={functionError}>My test</div>
                         </li>
                     </ul>
                 </nav>
