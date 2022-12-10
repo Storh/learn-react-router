6224d1d751fc65352d48b6c0fa79c8c084739a84
storh1118 <storh1118@outlook.com>
2022-12-10 23:32:46 +0800

    自定义某个路由的报错页面

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import Root, { loader as rootLoader, action as rootAction } from "./routes/root"
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
-import { action as destroyAction } from "./routes/destroy";
+import DeleteAgain, { action as destroyAction } from "./routes/destroy";
 
 const router = createBrowserRouter([
   {
@@ -37,6 +37,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId/destroy",
         action: destroyAction,
+        errorElement: <DeleteAgain />,
       },
     ],
   },
--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -2,6 +2,22 @@ import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts";
 
 export async function action({ params }) {
+    // 点击删除的时候随机出现错误
+    if (Math.floor(Math.random() * 10) % 2)
+        throw new Error("oh dang!");
     await deleteContact(params.contactId);
     return redirect("/");
 }
+
+const DeleteAgain = () => {
+    const backPage = () => {
+        history.back()
+    }
+    return (
+        <button onClick={backPage}>
+            返回上一页
+        </button>
+    )
+}
+
+export default DeleteAgain
